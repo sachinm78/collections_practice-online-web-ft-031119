2e236d16daf39fe8ba1b5dcb599eb69e10e51932
Sachin Mahajan <sachinm78@gmail.com>
2019-03-12 22:10:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,13 @@ def sort_array_asc(array)
   array.sort
 end
 
+
 def sort_array_desc(array)
-  array.sort |a, b|
+  array.sort do |a, b|
     b <=> a 
-  
+  end
+end
+
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
